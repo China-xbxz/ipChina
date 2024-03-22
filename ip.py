09be3473bec14a1af888cf1e3a807c7761cd2b7a
China-xbc <137941170+China-xb@users.noreply.github.com>
2024-03-23 04:45:23 +0800

    Update ip.py

--- ip.py
+++ ip.py
@@ -1,19 +1,20 @@
-# 导入所需的库
 import requests
+import output_files
 
 # URL
 url = "https://ispip.clang.cn/all_cn_cidr.txt"
 
-# 使用requests库从URL获取数据
+# 获取数据
 response = requests.get(url)
 
-# 通过检查响应状态码确认请求成功
+# 检查响应状态码
 if response.status_code == 200:
-    # 将请求的数据转换为文本
-    data = response.text.splitlines()
+    # 转换数据
+    converted_data = ['- IP-CIDR, ' + line for line in response.text.splitlines()]
 
-    # 开启新的文件并以写入模式打开
-    with open('Chinaip.txt', 'w') as f:
-        #通过遍历数据列表，我们可以通过使用字符串方法添加' - IP-CIDR,'前缀
-        for line in data:
-            f.write('- IP-CIDR,' + line + 'n')
+    # 使用output_files写入文件
+    with output_files.write('Chinaip.txt') as f:
+        f.writelines(converted_data)
+
+    # 打印成功消息
+    print("成功将转换后的数据保存到Chinaip.txt文件中。")
